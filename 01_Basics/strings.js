7c7252fee3a73dccffeba14860cede7a3aa2ff6e
TANU YADAV <105559613+tanuu2905@users.noreply.github.com>
2025-02-09 19:32:18 +0000

    Basics-01

---
+++ 01_Basics/strings.js
@@ -0,0 +1,28 @@
+const name = "Tanu"
+const age = 22
+
+console.log(`My name is ${name} and i am ${age} years old`);
+const string = new String("Tanuyadav")
+console.log(string[5])
+console.log(string.length);
+console.log(string.toUpperCase());
+console.log(string.charAt(4));
+console.log(string.indexOf('a'));
+ 
+const newstring = string.substring(1,5);
+console.log(newstring);
+
+const newstring2 = string.slice(3, 8)
+console.log(newstring2);
+
+const newstring3 = string.slice(-7, 5)
+console.log(newstring3);
+const newStringOne = "   Tanu    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+
+
+
+
+
